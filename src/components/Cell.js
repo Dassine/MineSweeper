7d36943161753b6cb72007be4feaf90cb41fe8cd
Dassine <bl.dassine@gmail.com>
2019-10-18 07:39:49 -0400

    Added project structure and MS solution with hooks

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,79 +1,73 @@
-import React, { Component } from 'react'
-import { StyleSheet, View, TouchableOpacity, Image, Text } from 'react-native'
+import React, {
+  useState,
+  useEffect,
+  forwardRef,
+  useRef,
+  useImperativeHandle,
+} from 'react';
+import { StyleSheet, View, TouchableOpacity, Image, Text } from 'react-native';
 
-import Constants from '../rsc/utils/Constants'
-import Colors from '../rsc/utils/Colors'
-import Images from '../rsc/assets/Images'
+import Constants from '../rsc/utils/Constants';
+import Colors from '../rsc/utils/Colors';
+import Images from '../rsc/assets/Images';
 
-class Cell extends Component {
-  state = {
-    isRevealed: false,
-    isMine: Math.random() < 0.2,
-    neighbors: null
-  }
+const Cell = forwardRef((props, ref) => {
+  const [isRevealed, setIsRevealed] = useState(false);
+  const [isMine, setIsMine] = useState(Math.random() < 0.2);
+  const [neighbors, setNeighbors] = useState(null);
 
-  revealCell = () => {
-    const { isRevealed } = this.state
-
-    if (isRevealed) {
-      return
-    }
-
-    this.setState({
-      isRevealed: true
-    })
-  }
-
-  onReveal = userInitiated => {
-    const { isRevealed, isMine } = this.state
-    const { x, y, onReveal, onMine } = this.props
-
-    if (isRevealed || (!userInitiated && isMine)) {
-      return
-    }
-
-    this.setState(
+  const cellRef = useRef(ref);
+  useImperativeHandle(
+    ref,
+    () => (
+      (cellRef.current = ref),
       {
-        isRevealed: true
-      },
-      () => {
-        if (isMine) {
-          onMine()
-        } else {
-          onReveal(x, y)
-        }
+        isMine,
+        setIsRevealed,
+        setIsMine,
+        setNeighbors,
+        onReveal,
       }
-    )
-  }
+    ),
+    [isMine]
+  );
 
-  onReset = () => {
-    this.setState({
-      isRevealed: false,
-      isMine: Math.random() < 0.2,
-      neighbors: null
-    })
-  }
+  useEffect(() => {
+    if (isRevealed) {
+      const { x, y, onReveal, onMine } = props;
+      if (isMine) {
+        onMine();
+      } else {
+        onReveal(x, y);
+      }
+    }
+  }, [isRevealed]);
 
-  render () {
-    const { cell, cellRevealed, cellMined } = styles
-    const { isRevealed, isMine, neighbors } = this.state
-    const { mine } = Images
+  const onReveal = userInitiated => {
+    if (isRevealed || (!userInitiated && isMine)) {
+      return;
+    }
 
-    return !isRevealed ? (
-      <TouchableOpacity onPress={() => this.onReveal(true)}>
-        <View style={cell} />
-      </TouchableOpacity>
-    ) : (
-      <View style={[cell, cellRevealed]}>
-        {isMine ? (
-          <Image source={mine} style={cellMined} resizeMode='contain' />
-        ) : (
-          <Text>{neighbors}</Text>
-        )}
-      </View>
-    )
-  }
-}
+    setIsRevealed(true);
+  };
+
+  const { cell, cellRevealed, cellMined } = styles;
+  const { mine } = Images;
+
+  return !isRevealed ? (
+    <TouchableOpacity onPress={() => onReveal(true)} ref={cellRef}>
+      <View style={cell} />
+    </TouchableOpacity>
+  ) : (
+    <View style={[cell, cellRevealed]} ref={cellRef}>
+      {isMine ? (
+        <Image source={mine} style={cellMined} resizeMode="contain" />
+      ) : (
+        <Text>{neighbors}</Text>
+      )}
+    </View>
+  );
+});
 
 const styles = StyleSheet.create({
   cell: {
@@ -84,18 +78,18 @@ const styles = StyleSheet.create({
     borderTopColor: 'white',
     borderLeftColor: 'white',
     borderRightColor: Colors.mediumGray,
-    borderBottomColor: Colors.mediumGray
+    borderBottomColor: Colors.mediumGray,
   },
   cellRevealed: {
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
-    borderColor: Colors.mediumGray
+    borderColor: Colors.mediumGray,
   },
   cellMined: {
     width: Constants.CELL_SIZE / 2,
-    height: Constants.CELL_SIZE / 2
-  }
-})
+    height: Constants.CELL_SIZE / 2,
+  },
+});
 
-export default Cell
+export default Cell;
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { StyleSheet, View, Button } from 'react-native'
+import React from 'react';
+import { StyleSheet, View, Button } from 'react-native';
 
-import Colors from '../rsc/utils/Colors'
-import Constants from '../rsc/utils/Constants'
-import Cell from './Cell'
+import Colors from '../rsc/utils/Colors';
+import Constants from '../rsc/utils/Constants';
+import Cell from './Cell';
 
 const Grid = () => {
   grid = Array.apply(null, Array(Constants.GRID_SIZE)).map(() => {
     return Array.apply(null, Array(Constants.GRID_SIZE)).map(() => {
-      return null
-    })
-  })
+      return null;
+    });
+  });
 
   getNeighborCells = (x, y) => {
-    let neighborCells = []
+    let neighborCells = [];
     for (let i = -1; i <= 1; i++) {
       for (let j = -1; j <= 1; j++) {
         if (
@@ -21,51 +21,53 @@ const Grid = () => {
           (x + i >= 0 && x + i <= Constants.GRID_SIZE - 1) &&
           (y + j >= 0 && y + j <= Constants.GRID_SIZE - 1)
         ) {
-          neighborCells.push(grid[x + i][y + j])
+          neighborCells.push(grid[x + i][y + j]);
         }
       }
     }
-    return neighborCells
-  }
+    return neighborCells;
+  };
 
   onReveal = (x, y) => {
-    let neighbors = 0
-    let cells = getNeighborCells(x, y)
+    let neighbors = 0;
+    let cells = getNeighborCells(x, y);
 
     cells.filter(elt => {
-      if (elt.state.isMine) {
-        neighbors++
+      if (elt.isMine) {
+        neighbors++;
       }
-    })
+    });
 
     if (neighbors) {
-      grid[x][y].setState({
-        neighbors: neighbors
-      })
+      grid[x][y].setNeighbors(neighbors);
     } else {
-      cells.map(elt => elt.onReveal(false))
+      cells.map(elt => {
+        elt.onReveal(false);
+      });
     }
-  }
+  };
 
   onMine = () => {
     grid.map(elt => {
-      elt.map(subElt => {
-        subElt.revealCell()
-      })
-    })
-  }
+      elt.map(elt => {
+        elt.setIsRevealed(true);
+      });
+    });
+  };
 
   resetGame = () => {
     grid.map(elt => {
-      elt.map(subElt => {
-        subElt.onReset()
-      })
-    })
-  }
+      elt.map(elt => {
+        elt.setIsRevealed(false);
+        elt.setIsMine(Math.random() < 0.2);
+        elt.setNeighbors(null);
+      });
+    });
+  };
 
   renderGrid = () => {
-    return grid.map((el, rowIndex) => {
-      let cellList = grid.map((el, colIndex) => {
+    return grid.map((elt, rowIndex) => {
+      let cellList = grid.map((elt, colIndex) => {
         return (
           <Cell
             key={colIndex}
@@ -74,46 +76,46 @@ const Grid = () => {
             onReveal={onReveal}
             onMine={onMine}
             ref={ref => {
-              grid[colIndex][rowIndex] = ref
+              grid[colIndex][rowIndex] = ref;
             }}
           />
-        )
-      })
-      const { cellContainer } = styles
+        );
+      });
+      const { cellContainer } = styles;
       return (
         <View key={rowIndex} style={cellContainer}>
           {cellList}
         </View>
-      )
-    })
-  }
+      );
+    });
+  };
 
-  const { container, gridContainer } = styles
+  const { container, gridContainer } = styles;
   return (
     <View style={container}>
       <View style={gridContainer}>{renderGrid()}</View>
-      <Button title='Start A New Game ' color='black' onPress={resetGame} />
+      <Button title="Start A New Game " color="black" onPress={resetGame} />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   gridContainer: {
     backgroundColor: Colors.mediumGray,
     flexDirection: 'column',
     width: Constants.GRID_WIDTH,
-    height: Constants.GRID_WIDTH
+    height: Constants.GRID_WIDTH,
   },
   cellContainer: {
     flexDirection: 'row',
     width: Constants.GRID_WIDTH,
-    height: Constants.CELL_SIZE
-  }
-})
+    height: Constants.CELL_SIZE,
+  },
+});
 
-export default Grid
+export default Grid;
